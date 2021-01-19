80479cd52628f8fdd3e730642f352732e2f44758
Jason <jeuxdeau2009@gmail.com>
2021-01-19 12:39:14 -0500

    resetting and confirming user input

--- components/Header.js
+++ components/Header.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import { View, Text, StyleSheet } from 'react-native';
 
+import Colors from '../constants/colors';
 
-
-const Header = (props) => {
+const Header = props => {
   return (
     <View style={styles.header}>
       <Text style={styles.headerTitle}>{props.title}</Text>
@@ -16,14 +16,14 @@ const styles = StyleSheet.create({
     width: '100%',
     height: 90,
     paddingTop: 36,
-    backgroundColor: '#f7287b',
+    backgroundColor: Colors.primary,
     alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: 'center'
   },
   headerTitle: {
     color: 'black',
-    fontSize: 18,
-  },
+    fontSize: 18
+  }
 });
 
 export default Header;
